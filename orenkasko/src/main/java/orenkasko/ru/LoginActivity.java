2216ae585a647b958301a92a224eaf43be8845f5
Anisimov Alexandr <trbrmrdr@gmail.com>
2017-11-14 06:42:52 +0300

    stage

--- orenkasko/src/main/java/orenkasko/ru/LoginActivity.java
+++ orenkasko/src/main/java/orenkasko/ru/LoginActivity.java
@@ -3,47 +3,30 @@ package orenkasko.ru;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.provider.SyncStateContract;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.telephony.PhoneNumberUtils;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import orenkasko.ru.Utils.Helpers;
 
-import static android.Manifest.permission.READ_CONTACTS;
 import static orenkasko.ru.Utils.Helpers.Toast;
 
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+public class LoginActivity extends AppCompatActivity {
 
 
     private static final String TAG = "LoginActivity";
@@ -52,19 +35,19 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         Log.e(TAG, msg);
     }
 
-    private static final int REQUEST_READ_CONTACTS = 0;
-
     private UserLoginTask mAuthTask = null;
 
     // UI references.
     private View mProgressView;
 
-    private View mPhoneForm;
-    private EditText mPhoneView;
+    private long mTime_send_sms = -1;
+    private int mCurrStage = -1;
+    private View mPhoneView;
+    private EditText mPhoneText;
 
-
-    //private View mLoginVerifyFormView;
-    //private EditText mPhoneCodeView;
+    private View mPhoneCodeView;
+    private EditText mPhoneCodeText;
+    private TextView mPhoneCodeReplayText;
 
 
     @Override
@@ -72,18 +55,34 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
-        mPhoneView = (EditText) findViewById(R.id.phone);
+        mPhoneView = findViewById(R.id.phone_form);
+        mPhoneText = (EditText) findViewById(R.id.phone);
+        mPhoneText.addTextChangedListener(new Helpers.TextWatcher_Phone(this, mPhoneText));
+        mPhoneText.setSelection(mPhoneText.getText().length());
+        mPhoneText.clearFocus();
 
-        mPhoneView.setOn
-
-        //maybe todo rad contacts and afto sets
-        //populateAutoComplete();
-
+        mPhoneCodeView = findViewById(R.id.phone_pass_form);
+        mPhoneCodeText = (EditText) findViewById(R.id.phone_pass);
+        mPhoneCodeReplayText = (TextView) findViewById(R.id.replay_pass_textview);
+        mPhoneCodeReplayText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                long delay = (4 * 60 * 1000) - (System.currentTimeMillis() - mTime_send_sms);
+                if (delay <= 0) {
+                    mCurrStage = 0;
+                    attemptLogin();
+                } else {
+                    SimpleDateFormat sdf = new SimpleDateFormat("mm:ss");
+                    Date resultdate = new Date(delay);
+                    Toast(LoginActivity.this, "отпраивть можно тольлко через " + sdf.format(resultdate));
+                }
+            }
+        });
         /*
         mPasswordView = (EditText) findViewById(R.id.password);
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+            public boolean onEditorAction(TextView newTextView, int id, KeyEvent keyEvent) {
                 if (id == R.id.login || id == EditorInfo.IME_NULL) {
                     attemptLogin();
                     return true;
@@ -94,105 +93,134 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 */
 
 
-
-
-
         Button mSignNextButton = (Button) findViewById(R.id.sign_next_button);
-        mSignNextButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptLogin();
-            }
-        });
+        mSignNextButton.setOnClickListener(new View.OnClickListener() {
+                                               @Override
+                                               public void onClick(View v) {
+                                                   attemptLogin();
+                                               }
+                                           }
 
+        );
+
+        //mCurrStage = 0;
+        setCurrStage(true);
         mProgressView = findViewById(R.id.login_progress);
+        getWindow().getDecorView().clearFocus();
+        mProgressView.setFocusable(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        mCurrStage -= 1;
+        if (mCurrStage >= -1) {
+            setCurrStage(false);
+        } else {
+            finish();
+            //super.onBackPressed();
+        }
 
     }
 
-    private void populateAutoComplete() {
-        if (!mayRequestContacts()) {
+    private void setCurrStage(Boolean success) {
+        if (mCurrStage == -1) {
+
+            mPhoneView.setVisibility(View.VISIBLE);
+            mPhoneCodeView.setVisibility(View.GONE);
+
+            mPhoneText.setFocusable(false);
+            mPhoneText.setText("");
+
+            new Handler(this.getMainLooper()).postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mPhoneText.setText(LoginActivity.this.getText(R.string.phone_start_text));
+
+                    mPhoneText.setFocusable(true);
+                    mPhoneText.setFocusableInTouchMode(true);
+                }
+            }, 1000L);
+            mCurrStage += 1;
             return;
         }
 
-        getLoaderManager().initLoader(0, null, this);
-    }
+        if (mCurrStage == 0) {
+            if (success) {
+                mTime_send_sms = System.currentTimeMillis();
+                Toast(LoginActivity.this, "пароль отправлен");
+                mCurrStage += 1;
 
-    private boolean mayRequestContacts() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true;
-        }
+                mPhoneView.setVisibility(View.GONE);
+                mPhoneCodeView.setVisibility(View.VISIBLE);
 
-        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        }
-        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-            Snackbar.make(mPhoneView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new View.OnClickListener() {
-                        @Override
-                        @TargetApi(Build.VERSION_CODES.M)
-                        public void onClick(View v) {
-                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-                        }
-                    });
-        } else {
-            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-        }
-        return false;
-    }
-
-    /**
-     * Callback received when a permissions request has been completed.
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_READ_CONTACTS) {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                populateAutoComplete();
+                mPhoneCodeText.setFocusable(false);
+                new Handler(this.getMainLooper()).postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mPhoneCodeText.setFocusable(true);
+                        mPhoneCodeText.setFocusableInTouchMode(true);
+                    }
+                }, 1000L);
+                return;
+            } else {
+                mPhoneText.setError(getString(R.string.error_incorrect_phone));
+                mPhoneText.requestFocus();
+                return;
             }
         }
+
+        if (mCurrStage == 1) {
+            Intent intent = new Intent(this, MainActivity.class);
+            this.startActivity(intent);
+        }
     }
 
-    /**
-     * Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
     private void attemptLogin() {
         if (mAuthTask != null) {
             return;
         }
         // Reset errors.
-        mPhoneView.setError(null);
-
-        // Store values at the time of the login attempt.
-        String phone = mPhoneView.getText().toString();
-
+        mPhoneText.setError(null);
         View focusView = null;
 
-        // Check for a valid password, if the user entered one.
-        if (TextUtils.isEmpty(phone)) {
-            mPhoneView.setError(getString(R.string.error_field_required));
-            focusView = mPhoneView;
+        if (mCurrStage == 0) {
+            String phone = mPhoneText.getText().toString();
+            if (TextUtils.isEmpty(phone)) {
+                mPhoneText.setError(getString(R.string.error_field_required));
+                focusView = mPhoneText;
 
-        } else if (!isPhoneValid(phone)) {
-            mPhoneView.setError(getString(R.string.error_invalid_password));
-            focusView = mPhoneView;
+            } else if (!isPhoneValid(phone)) {
+                mPhoneText.setError(getString(R.string.error_invalid_phone));
+                focusView = mPhoneText;
+            }
+        } else if (mCurrStage == 1) {
+            String phone_pass = mPhoneCodeText.getText().toString();
+            if (TextUtils.isEmpty(phone_pass)) {
+                mPhoneCodeText.setError(getString(R.string.error_field_required));
+                focusView = mPhoneCodeText;
 
+            } else if (!isPhonePassValid(phone_pass)) {
+                mPhoneCodeText.setError(getString(R.string.error_invalid_phone_password));
+                focusView = mPhoneCodeText;
+            }
         }
 
         if (null != focusView) {
             focusView.requestFocus();
         } else {
             showProgress(true);
-            mAuthTask = new UserLoginTask(phone);
+
+            mAuthTask = new UserLoginTask();
             mAuthTask.execute((Void) null);
         }
     }
 
     private boolean isPhoneValid(String phone) {
-        //TODO: Replace this with your own logic
-        return phone.length() > 10;
+        return phone.length() == 17;
+    }
+
+    private boolean isPhonePassValid(String phone_pass) {
+        return phone_pass.length() >= 4;
     }
 
     /**
@@ -212,6 +240,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 @Override
                 public void onAnimationEnd(Animator animation) {
                     mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+
                 }
             });
         } else {
@@ -219,66 +248,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // and hide the relevant UI components.
             mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
         }
-    }
-
-
-    // These are the Contacts rows that we will retrieve.
-    static final String[] CONTACTS_SUMMARY_PROJECTION = new String[]{
-            ContactsContract.Contacts._ID,
-            /*ContactsContract.Contacts.DISPLAY_NAME,
-            ContactsContract.Contacts.CONTACT_STATUS,
-            ContactsContract.Contacts.CONTACT_PRESENCE,
-            ContactsContract.Contacts.PHOTO_ID,
-            ContactsContract.Contacts.LOOKUP_KEY,
-            ContactsContract.CommonDataKinds.Phone.NUMBER,
-            */
-    };
-
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        Uri baseUri = ContactsContract.Contacts.CONTENT_URI;
-
-        // Now create and return a CursorLoader that will take care of
-        // creating a Cursor for the data being displayed.
-        String select = "((" + ContactsContract.Contacts.DISPLAY_NAME + " NOTNULL) AND ("
-                + ContactsContract.Contacts.HAS_PHONE_NUMBER + "=1) AND ("
-                + ContactsContract.Contacts.DISPLAY_NAME + " != '' ))";
-        return new CursorLoader(LoginActivity.this, baseUri,
-                CONTACTS_SUMMARY_PROJECTION, select, null,
-                ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC");
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-        List<String> phones = new ArrayList<>();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            String contact_id = cursor.getString(0);
-
-            Cursor t_phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contact_id, null, null);
-            while (t_phones.moveToNext()) {
-                String phoneNumber = t_phones.getString(t_phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                //Log("Number" + phoneNumber);
-                phones.add(phoneNumber);
-            }
-            t_phones.close();
-
-            cursor.moveToNext();
-        }
-
-        addPhonessToAutoComplete(phones);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader) {
-    }
-
-    private void addPhonessToAutoComplete(List<String> phonesCollection) {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(LoginActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, phonesCollection);
-
-        mPhoneView.setAdapter(adapter);
+        getWindow().getDecorView().clearFocus();
+        mProgressView.setFocusable(show);
+        mPhoneText.setFocusable(!show);
+        mPhoneCodeText.setFocusable(!show);
     }
 
 
@@ -288,10 +261,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
-        private final String mPhone;
-
-        UserLoginTask(String phone) {
-            mPhone = phone;
+        UserLoginTask() {
         }
 
         @Override
@@ -313,14 +283,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         @Override
         protected void onPostExecute(final Boolean success) {
             mAuthTask = null;
-            showProgress(false);
+            setCurrStage(success);
 
-            if (success) {
-                Toast(LoginActivity.this, "succes");
-            } else {
-                mPhoneView.setError(getString(R.string.error_incorrect_phone));
-                mPhoneView.requestFocus();
-            }
+            showProgress(false);
         }
 
         @Override
--- orenkasko/src/main/java/orenkasko/ru/Utils/Helpers.java
+++ orenkasko/src/main/java/orenkasko/ru/Utils/Helpers.java
@@ -1,17 +1,189 @@
 package orenkasko.ru.Utils;
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import orenkasko.ru.R;
 
 /**
  * Created by trbrm on 13.11.2017.
  */
 
 public class Helpers {
+    private final static String TAG = "Helpers";
+
+    private final static void Log(final String msg) {
+        Log.e(TAG, msg);
+    }
 
 
     public static void Toast(final Context context, final String msg) {
         Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+    }
+
+
+    public static class TextWatcher_Phone implements TextWatcher {
+
+        private Context mContext;
+        private EditText mPhoneText;
+
+        public TextWatcher_Phone(final Context context, EditText phoneText) {
+            mContext = context;
+            mPhoneText = phoneText;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        String mLastText = "";
+        int mSelectPos = -1;
+        boolean mHasAddToLast = false;
+        boolean mTextErace = false;
+
+        String erace(String str, int pos) {
+            return str.substring(0, pos) + str.substring(pos + 1, str.length());
+        }
+
+        String insert(String str, int pos, char ch) {
+            return str.substring(0, pos) + ch + str.substring(pos, str.length());
+        }
+
+        String getNumber(String str) {
+            char _ch[] = {'(', ')', '-', '+'};
+            String ret = str;
+            for (char ch : _ch) {
+                ret = ret.replace(ch + "", "");
+            }
+            return ret;
+        }
+
+        String setFromFormat(String phone) {
+            String newText = phone.toString();
+            int lenght = newText.length();
+
+            if (lenght > 0 && newText.charAt(0) != '+') {
+                newText = "+" + newText;
+                lenght = newText.length();
+            }
+
+            if (lenght > 2 && newText.charAt(2) != '(') {
+                newText = insert(newText, 2, '(');
+                lenght = newText.length();
+            }
+            if (lenght > 6 && newText.charAt(6) != ')') {
+                newText = insert(newText, 6, ')');
+                lenght = newText.length();
+            }
+            //7 11 14
+            int t_pos[] = {7, 11, 14};
+            for (int _pos : t_pos) {
+                if (lenght > _pos && newText.charAt(_pos) != '-') {
+                    newText = insert(newText, _pos, '-');
+                    lenght = newText.length();
+                }
+            }
+
+            //__________
+
+            mHasAddToLast = false;
+            if (lenght == 0) {
+                newText = "+";
+                mHasAddToLast = true;
+            } else if (lenght == 2) {
+                newText += "(";
+                mHasAddToLast = true;
+            } else if (lenght == 6) {
+                newText += ")";
+                lenght += 1;
+                mHasAddToLast = true;
+            }
+            if (lenght == 7 || lenght == 11 || lenght == 14) {
+                newText += "-";
+                mHasAddToLast = true;
+            }
+
+            return newText;
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (s.length() <= 0) return;
+            String newText = s.toString();
+            if (newText.length() < 3) {
+                newText = mContext.getString(R.string.phone_start_text);
+            }
+
+            if (mLastText.length() == 0 || !newText.equals(mLastText))
+                mSelectPos = mPhoneText.getSelectionStart();
+
+            String phoneString = getNumber(newText);
+            newText = setFromFormat(phoneString);
+
+
+            if (s.length() < mLastText.length()) {
+                mTextErace = true;
+                if (newText.equals(mLastText)) {
+                    Log(mSelectPos + "");
+
+                    if (mSelectPos != 0 || mSelectPos != 2) {
+                        int posErace = phoneString.length() - 1;
+                        if (mSelectPos == 6 || mSelectPos == 7) {
+                            posErace = 3;
+                            mSelectPos = 5;
+                        } else if (mSelectPos == 11) {
+                            posErace = 6;
+                            mSelectPos = 10;
+                        } else if (mSelectPos == 14) {
+                            posErace = 8;
+                            mSelectPos = 13;
+                        }
+                        if (phoneString.length() > 1) {
+                            phoneString = erace(phoneString, posErace);
+                        } else {
+                            phoneString = "";
+                        }
+
+                        newText = setFromFormat(phoneString);
+                    } else {
+                        mTextErace = false;
+                    }
+                }
+            }
+
+
+            //+7(981)-699-18-77
+            Log(mLastText + " " + newText);
+            if (null == mLastText || !newText.equals(mLastText)) {
+                mLastText = newText;
+                mPhoneText.setText(newText);
+                if (!mHasAddToLast) {
+                    if (mSelectPos == -1 || mSelectPos > newText.length())
+                        mSelectPos = newText.length();
+                    mPhoneText.setSelection(mSelectPos);
+                }
+            }
+            if (mHasAddToLast) {
+                if (mTextErace && newText.length() > mSelectPos) {
+                    mSelectPos = mSelectPos;
+                    mTextErace = false;
+                } else if (newText.length() >= 3)
+                    mSelectPos = newText.length();
+                mPhoneText.setSelection(mSelectPos);
+                mLastText = newText;
+            }
+            return;
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
     }
 
 
